0e14ae37fe76bf0e7452d0eab278e34a3bf3704d
leocandido <leandro.candido@yahoo.com.br>
2019-04-28 10:05:19 -0300

    add reservation unit tests

---
+++ test/Ryanair.Reservation.Tests/Domain/Entities/ReservationAggregate/ReservationTest.cs
@@ -0,0 +1,49 @@
+﻿using Ryanair.Reservation.Domain.ValueObjects;
+using Xunit;
+
+namespace Ryanair.Reservation.Tests.Domain.Entities.ReservationAggregate
+{
+    public class ReservationTest
+    {
+
+        [Fact]
+        public void CandAddReservationOk()
+        {
+
+            var problems = Ryanair.Reservation.Domain.Entities.ReservationAggregate.Reservation.CanCreateReservation(GetReservationData());
+
+            Assert.Empty(problems);
+        }
+
+        [Fact]
+        public void ReservationEmailInvalid()
+        {
+            var reservationData = GetReservationData();
+            reservationData.Email = null;
+            var problems = Ryanair.Reservation.Domain.Entities.ReservationAggregate.Reservation.CanCreateReservation(reservationData);
+
+            Assert.NotEmpty(problems);
+        }
+
+        [Fact]
+        public void ReservationCreditCardInvalid()
+        {
+
+            var reservationData = GetReservationData();
+            reservationData.CreditCard = null;
+            var problems = Ryanair.Reservation.Domain.Entities.ReservationAggregate.Reservation.CanCreateReservation(reservationData);
+
+            Assert.NotEmpty(problems);
+        }
+
+        private ReservationData GetReservationData()
+        {
+            return new ReservationData
+            {
+                Email = "leandro@gmail.com",
+                CreditCard = "123456789"
+            };
+
+        }
+    }
+}
---
+++ test/Ryanair.Reservation.Tests/Domain/Services/ReservationServiceTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Ryanair.Reservation.Domain.Entities.FlightAggregate;
+using Ryanair.Reservation.Domain.Interfaces;
+using Ryanair.Reservation.Domain.Interfaces.Services;
+using Ryanair.Reservation.Domain.Services;
+using Ryanair.Reservation.Domain.Validation;
+using Ryanair.Reservation.Domain.ValueObjects;
+using Ryanair.Reservation.Infrastructure.Repositories;
+using Xunit;
+
+namespace Ryanair.Reservation.Tests.Domain.Services
+{
+    public class ReservationServiceTest
+    {
+        [Fact]
+        public void CreatingReservationOk()
+        {
+            IRepository<Flight> _flightRepository = new FlightRepository();
+            IReservationService service = new ReservationService(_flightRepository);
+            var result = service.ConfirmReservation(GetEntityValidEntity());
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void CreatingReservationNoFlightKeyInformationError()
+        {
+            IRepository<Flight> _flightRepository = new FlightRepository();
+            IReservationService service = new ReservationService(_flightRepository);
+
+            var data = GetEntityValidEntity();
+            data.Flights[0].Key = "";
+
+            Assert.Throws<DomainValidationException>(() => service.ConfirmReservation(data));
+        }
+
+        [Fact]
+        public void CreatingReservationNoFlightInformationError()
+        {
+            IRepository<Flight> _flightRepository = new FlightRepository();
+            IReservationService service = new ReservationService(_flightRepository);
+
+            var data = GetEntityValidEntity();
+            data.Flights = null;
+
+            Assert.Throws<DomainValidationException>(() => service.ConfirmReservation(data));
+        }
+
+
+        private ReservationData GetEntityValidEntity()
+        {
+            return new ReservationData()
+            {
+                Email = "contact@contact.com",
+                CreditCard = "0123456789012345",
+                Flights = new List<FlightData>()
+                {
+                    new FlightData
+                    {
+                        Key = "Flight00052",
+                        Passengers = new List<PassengerData>()
+                        {
+                            new PassengerData
+                            {
+                                Name = "Robert Plant",
+                                Bags = 2,
+                                Seat = "27"
+                            },
+                            new PassengerData
+                            {
+                                Name = "Ozzy Osbourne",
+                                Bags = 0,
+                                Seat = "28"
+                            }
+                        }
+                    },
+                    new FlightData
+                    {
+                        Key = "Flight00103",
+                        Passengers = new List<PassengerData>()
+                        {
+                            new PassengerData
+                            {
+                                Name = "Robert Plant",
+                                Bags = 2,
+                                Seat = "41"
+                            },
+                            new PassengerData
+                            {
+                                Name = "Ozzy Osbourne",
+                                Seat = "40"
+                            }
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
