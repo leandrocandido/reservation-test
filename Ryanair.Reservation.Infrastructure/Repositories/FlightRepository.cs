3f8a0fb6c71520c970f8a6061ca827211551ee06
Leandro Evangelista <leandro.evangelista@btgpactual.com>
2019-04-27 23:33:41 -0300

    Initial data

--- Ryanair.Reservation.Infrastructure/Repositories/FlightRepository.cs
+++ Ryanair.Reservation.Infrastructure/Repositories/FlightRepository.cs
@@ -1,4 +1,10 @@
 ﻿
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
 namespace Ryanair.Reservation.Infrastructure.Repositories
 {
     /// <summary>
@@ -8,5 +14,29 @@ namespace Ryanair.Reservation.Infrastructure.Repositories
     /// Having separated repositories can help to build a cache layer or add some custom behavior.
     /// </remarks>
     public class FlightRepository : RepositoryBase<Domain.Entities.Flight>
-    { }
+    {
+        public FlightRepository()
+        {
+            this.Seed();
+        }
+
+        private void Seed()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Ryanair.Reservation.Infrastructure.InitialState.json";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string json = reader.ReadToEnd();
+                var initialList = JsonConvert.DeserializeObject<List<Domain.Entities.Flight>>(json, new JsonSerializerSettings
+                {
+                    ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                });
+
+                this.collection.AddRange(initialList);
+            }
+        }
+    }
 }
